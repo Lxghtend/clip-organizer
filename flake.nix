c1797705968d45cf6559f1811c8fa743345807c5
Lxghtend <140092221+Lxghtend@users.noreply.github.com>
2026-07-06 14:30:36 -0400

    Update nixpkgs URL to version 26.05

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.buildGoModule {
       pname = "clip-organizer";
